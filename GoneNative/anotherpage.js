184072397918ca476f9a1c53451caa07044865f0
aphilipps <aphilipps9@gmail.com>
2016-08-11 15:47:28 -0400

    Finished Workshop

---
+++ GoneNative/anotherpage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { AppRegistry, View, Text, StyleSheet, NavigatorIOS } from 'react-native';
+
+
+class AnotherPage extends Component {
+  render() {
+    return (
+      <View style={styles.viewContainer}>
+        <Text style={styles.text}>
+          This is some filler text to tell you that youve
+          navigated to another page.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  button: {
+    marginTop: 30,
+    fontSize: 50,
+  },
+  text: {
+    color: 'green',
+    fontSize: 40,
+  },
+});
+
+
+export default AnotherPage
